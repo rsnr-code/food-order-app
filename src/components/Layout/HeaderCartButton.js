0b80098cf6a6946d39574ff8b4e3e88d597ca346
rsnr-code <rishadnero@gmail.com>
2022-12-07 13:33:20 -0500

    added remove item handler

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon'
 
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0);
+
     return ( 
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
@@ -12,7 +20,7 @@ const HeaderCartButton = (props) => {
                 Your Cart
             </span>
             <span className={classes.badge}>
-                3
+                {numOfCartItems}
             </span>
         </button>
      );
